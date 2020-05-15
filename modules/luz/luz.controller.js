c884ed31dfc9c0e99ecd3dff4b26b174d9a8ba05
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-05-15 18:26:58 -0300

    true e false substituido por int

--- modules/luz/luz.controller.js
+++ modules/luz/luz.controller.js
@@ -4,9 +4,9 @@ exports.luzStatus = async (req, res) => {
   var minutos = date.getMinutes();
   let status = ''
   if(hora >= 7 && hora <= 19) {
-    status = true;
+    status = 1;
   } else {
-    status = false
+    status = 0
   }
   return res.status(200).json({ status: 200, statusLuz: status, hora: hora, minutos: minutos });
 }
